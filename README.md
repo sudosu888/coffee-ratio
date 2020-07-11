25107006fa332bcfa269d43df31366f2bf51d5eb
sudosu888 <zebbieman123@gmail.com>
2020-07-11 09:57:35 -0600

    Uploaded my program

---
+++ README.md
@@ -0,0 +1,6 @@
+#Coffee Ratio Calculator
+
+I use this little handy C program to calculate how much coffee grams I need
+based on how much coffee I want, and vice versa. Read the source code if you need
+to figure out the syntax. It's small and really easy and I have the inteneded syntax in the
+comments.
---
+++ coffeeratio.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	/*
+	 * Intended syntax: 'ratio <coffee> <water> <amount> <arguments>'
+	 * for example, if you want a ratio of 1:16 with 650 grams of water
+	 * the user would type: 'ratio 1 16 650'
+	 */
+
+	if(argc >= 4)
+	{
+    char *str_coffee = argv[1];
+		char *str_water = argv[2];
+		char *str_amount = argv[3];
+
+		/* Convert string to integer */
+		int arg;
+  	int coffee = atoi(str_coffee);
+		int water = atoi(str_water);
+		int amount = atoi(str_amount);
+		int result;
+
+		if(coffee > 1)
+			printf("WARNING: ratio irregular, calculations may not be accurate!\n");
+
+		while((arg = getopt(argc, argv, "c")) != -1)
+		{
+			switch(arg)
+			{
+				case 'c' :
+					result = amount * water;
+					printf("Ratio %d:%d = %dg of water\n", coffee, water, result);
+					return 0;
+
+				default :
+					break;
+			}
+		}
+
+		result = amount / water;
+		printf("Ratio %d:%d = %dg of coffee\n", coffee, water, result);
+	}
+}
